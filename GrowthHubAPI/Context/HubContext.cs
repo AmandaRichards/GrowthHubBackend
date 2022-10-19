549a1045a773e45fca9448fd99317fdc4f18e964
Amanda Richards <amandarichards@soliriusadmins-MacBook-Pro-3.local>
2022-10-19 14:58:11 +0100

    context added

---
+++ GrowthHubAPI/Context/HubContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace GrowthHubAPI.Context
+{
+    public class HubContext
+    {
+        public HubContext()
+        {
+        }
+    }
+}
+
---
+++ GrowthHubAPI/Models/Subject.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace GrowthHubAPI.Models
+{
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+
+    [Table("Subject")]
+    public class Subject
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int SubjectId { get; set; }
+
+        public string SubjectName { get; set; } = String.Empty;
+
+        public string SubjectImage { get; set; } = String.Empty;
+
+        public List<string>? ResourceLink { get; set; }
+
+    }
+}
+
