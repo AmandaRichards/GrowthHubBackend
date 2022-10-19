c4d5de75713f693d424ab92660c15e07077e6a28
Amanda Richards <amandarichards@soliriusadmins-MacBook-Pro-3.local>
2022-10-19 15:13:40 +0100

    runs again

--- GrowthHubAPI/Context/HubContext.cs
+++ GrowthHubAPI/Context/HubContext.cs
@@ -1,11 +1,19 @@
 ﻿using System;
+using GrowthHubAPI.Models;
+
 namespace GrowthHubAPI.Context
 {
+    using Microsoft.EntityFrameworkCore;
+    //using Microsoft.WebApiCoreWithEF.Models;
+
     public class HubContext
+        :DbContext
     {
-        public HubContext()
+        public HubContext(DbContextOptions options)
+            : base(options)
         {
         }
+        public DbSet<Subject>? Subjects { get; set; }
     }
 }
 
